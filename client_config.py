9aa92b67f6dd46c140a5d72d894ec25b18b15e65
wisehackermonkey <oranbusiness@gmail.com>
2024-09-22 14:08:52 -0700

    removed_old coded

--- client_config.py
+++
@@ -1,8 +0,0 @@
-class ClientConfig(object):
-    PUBLIC_KEY = 'B7Dqz+/lLGNvVJwFb8LgVt+R5VnnuettyXatkPctGsQ'
-    APP_NAME = 'ultimate-tts-reader'
-    COMPANY_NAME = 'ultimate-tts-reader'
-    HTTP_TIMEOUT = 30
-    MAX_DOWNLOAD_RETRIES = 3
-    UPDATE_URLS = ['http://64.227.84.118:7777/']
-    APP_VERSION = "1.4.0"
